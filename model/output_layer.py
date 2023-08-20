9eb86b6cb9f13f49c0f9bf4de8c146eb49bd8173
Andrew Santhosh <andrewsanthosh264@gmail.com>
2023-08-20 22:09:42 +0530

    OutputLayer: clean up

--- model/output_layer.py
+++ model/output_layer.py
@@ -7,30 +7,21 @@ class SharedOutput(Layer):
     This is a custom output layer that shares its weights with an embedding layer.
     """
 
-    def __init__(self, embedding_layer: Embedding, activation: str = None, **kwargs):
+    def __init__(self, embedding_layer: Embedding, **kwargs):
         super().__init__(**kwargs)
         self.embedding_layer = embedding_layer
-        self.activation = tf.keras.activations.get(activation)
-
-    def build(self, input_shape):
-        self.e_weights = tf.transpose(self.embedding_layer.weights[0])
-        self.built = True
 
     def update_weights(self, embedding: Embedding):
         """
         Updates the weights of the output layer to be the same as the embedding layer.
-
-        :param embedding: (Embedding) The embedding layer.        
+        :param embedding: (Embedding) The embedding layer.
         """
         self.embedding_layer = embedding
-        self.e_weights = tf.transpose(self.embedding_layer.weights[0])
 
     def compute_output_shape(self, input_shape):
-        return input_shape[0], tf.shape(self.embedding_layer.weights[0])[0]
+        return input_shape[0], tf.shape(self.embedding_layer.embeddings)[0]
 
     def call(self, inputs, *args, **kwargs):
         inputs = tf.cast(inputs, dtype=tf.float32)
-        output = tf.matmul(inputs, self.e_weights)
-        if self.activation is not None:
-            output = self.activation(output)
+        output = tf.matmul(inputs, self.embedding_layer.embeddings, transpose_b=True, name="output_weights")
         return output
