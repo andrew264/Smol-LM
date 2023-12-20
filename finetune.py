Gradient Accumulation, Mixture of Experts, fix kv_cache

finetune.py
@@ -5,7 +5,7 @@ import time
 
 import numpy as np
 import torch
-import torch.nn.functional as F
+from accelerate import Accelerator
 from torch.utils.data import DataLoader, Dataset
 
 from model import ModelConfig, Transformer
@@ -39,34 +39,38 @@ def train(model, config: ModelConfig):
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, fused=True)
 
     accum_steps = config.grad_accumulation_steps
+    accelerator = Accelerator(gradient_accumulation_steps=accum_steps)
+    model, optimizer, training_dataloader = accelerator.prepare(model, optimizer, train_data)
 
     losses = []
     start_time = time.time()
     print(f"Training for {epochs} epochs...")
     print(f"Total number of batches: {len(dataloader)}")
+
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1} started.")
         for i, (x, y) in enumerate(dataloader):
-            x = x.to(device)
-            y = y.to(device)
-
-            with torch.set_grad_enabled(True):
-                logits = model(x)
-                loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1), ignore_index=-1)
+            # train step
+            with accelerator.accumulate(model):
+                x = x.to(device)
+                y = y.to(device)
+                logits, loss = model(x=x, y=y)
                 losses.append(loss.item())
-                loss = loss / accum_steps
-                loss.backward(retain_graph=True)
+                accelerator.backward(loss)
                 torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-                if (i + 1) % accum_steps == 0 or i == len(train_data) - 1:
-                    optimizer.step()
-                    optimizer.zero_grad()
+                optimizer.step()
+                optimizer.zero_grad()
 
             if (i + 1) % 100 == 0:
+                time_delta = time.time() - start_time
                 avg_loss = sum(losses) / len(losses)
                 avg_perplexity = torch.exp(torch.tensor(avg_loss))
-                print(f"Epoch {epoch + 1} | Batch {i + 1} | Loss {avg_loss:.3f} | Perplexity {avg_perplexity:.3f} | "
+                tokens_per_sec = 100 * config.max_batch_size * config.max_position_embeddings / time_delta
+                print(f"Step {i} | Loss {avg_loss:.3f} | Perplexity {avg_perplexity:.3f} | "
                       f"Bits/Token {avg_loss / np.log(2):.3f} | "
-                      f"Time {time.time() - start_time:.1f}s")
+                      f"Time {time_delta:.1f}s | "
+                      f"Tokens/s {tokens_per_sec:.1f}"
+                      )
                 start_time = time.time()
                 losses = []
             if i % 1000 == 0 and i > 0:
@@ -79,7 +83,7 @@ def train(model, config: ModelConfig):
 if __name__ == '__main__':
     if os.path.exists('./weights/config.json'):
         config = ModelConfig.from_json('./weights/config.json')
-        config.max_epochs = 3
+        config.max_epochs = 2
         print("Loaded config from file.")
     else:
         raise FileNotFoundError("No config file found.")
@@ -88,7 +92,10 @@ if __name__ == '__main__':
     model.to(dtype=torch.bfloat16, device=device)
     torch.compile(model=model.forward, fullgraph=True, mode='reduce-overhead')
 
-    if os.path.exists('./weights/model_ckpt.pt'):
+    if os.path.exists('./finetuned-weights/model_ckpt.pt'):
+        model.load_state_dict(torch.load('./finetuned-weights/model_ckpt.pt'))
+        print("Continuing training from file.")
+    elif os.path.exists('./weights/model_ckpt.pt'):
         model.load_state_dict(torch.load('./weights/model_ckpt.pt'))
         print("Loaded model from file.")
     else:

main.py
@@ -3,7 +3,7 @@ import time
 
 import numpy as np
 import torch
-import torch.nn.functional as F
+from accelerate import Accelerator
 from torch.utils.data import DataLoader, Dataset
 
 from model import ModelConfig, Transformer
@@ -38,8 +38,7 @@ def estimate_loss(model: Transformer, config: ModelConfig):
     for i, (x, y) in enumerate(validation_data):
         x = x.to(device)
         y = y.to(device)
-        logits = model(x)
-        loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1), ignore_index=-1)
+        logits, loss = model(x=x, y=y)
         losses.append(loss.item())
         if i >= 250:
             break
@@ -50,15 +49,14 @@ def estimate_loss(model: Transformer, config: ModelConfig):
     model.train()
 
 
-def train(model, config: ModelConfig):
+def train(model, optimizer, config: ModelConfig):
     # dataloader
     dataset = NPDataset(dataset_path, config.max_position_embeddings)
     train_data = DataLoader(dataset, batch_size=config.max_batch_size, shuffle=False, drop_last=True)
 
-    # optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, fused=True)
-
     accum_steps = config.grad_accumulation_steps
+    accelerator = Accelerator(gradient_accumulation_steps=accum_steps)
+    model, optimizer, training_dataloader = accelerator.prepare(model, optimizer, train_data)
 
     losses = []
     start_time = time.time()
@@ -71,27 +69,30 @@ def train(model, config: ModelConfig):
     for i, (x, y) in enumerate(train_data):
         if i <= step:
             continue
-        x = x.to(device)
-        y = y.to(device)
+        if i == step + 1:
+            start_time = time.time()
 
-        with torch.set_grad_enabled(True):
-            logits = model(x)
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1), ignore_index=-1)
+        # train step
+        with accelerator.accumulate(model):
+            x = x.to(device)
+            y = y.to(device)
+            logits, loss = model(x=x, y=y)
             losses.append(loss.item())
-            loss = loss / accum_steps
-            loss.backward(retain_graph=True)
+            accelerator.backward(loss)
             torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-            if (i + 1) % accum_steps == 0 or i == len(train_data) - 1:
-                optimizer.step()
-                optimizer.zero_grad()
+            optimizer.step()
+            optimizer.zero_grad()
 
-        losses.append(loss.item())
         if i % 100 == 0 and i > 0:
+            time_delta = time.time() - start_time
             avg_loss = sum(losses) / len(losses)
             avg_perplexity = torch.exp(torch.tensor(avg_loss))
+            tokens_per_sec = 100 * config.max_batch_size * config.max_position_embeddings / time_delta
             print(f"Step {i} | Loss {avg_loss:.3f} | Perplexity {avg_perplexity:.3f} | "
                   f"Bits/Token {avg_loss / np.log(2):.3f} | "
-                  f"Time {time.time() - start_time:.1f}s")
+                  f"Time {time_delta:.1f}s | "
+                  f"Tokens/s {tokens_per_sec:.1f}"
+                  )
             start_time = time.time()
             losses = []
         if i % 1000 == 0 and i > 0:
@@ -99,6 +100,7 @@ def train(model, config: ModelConfig):
             with open('./weights/step.txt', 'w') as f:
                 f.write(f"{i}\n")
             estimate_loss(model, config=config)
+            start_time = time.time()
     torch.save(model.state_dict(), f"./weights/model_ckpt.pt")
 
 
@@ -130,5 +132,8 @@ if __name__ == '__main__':
     else:
         print("Created new model.")
 
-    train(model, config=config)
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, fused=True)
+
+    train(model, optimizer, config=config)
     # estimate_loss(model, config=config)

model/attention_layer.py
@@ -2,95 +2,95 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
+from flash_attn.ops.fused_dense import FusedDense
 from torch import Tensor
 from torch.nn import functional as F
-
-from flash_attn.ops.fused_dense import FusedDense
 
 from model import ModelConfig
 
 
-def apply_rotary_emb(x: Tensor, freqs_cis: Tensor) -> Tensor:
-    xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
-    freqs_cis = freqs_cis.view(1, xshaped.size(1), 1, xshaped.size(3), 2)
-    x_out2 = torch.stack(
-        [
-            xshaped[..., 0] * freqs_cis[..., 0] - xshaped[..., 1] * freqs_cis[..., 1],
-            xshaped[..., 1] * freqs_cis[..., 0] + xshaped[..., 0] * freqs_cis[..., 1],
-        ],
-        -1,
-    )
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    """
+    Reshape frequency tensor for broadcasting it with another tensor.
+    """
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(*shape)
 
-    x_out2 = x_out2.flatten(3)
-    return x_out2.type_as(x)
+
+def apply_rotary_emb(xq: Tensor, xk: Tensor, freqs_cis: Tensor) -> tuple[Tensor, Tensor]:
+    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
+    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
+    return xq_out.type_as(xq), xk_out.type_as(xk)
 
 
 class Attention(nn.Module):
-    def __init__(self, config: ModelConfig):
+    def __init__(self, config: ModelConfig) -> None:
         super().__init__()
         self.config = config
 
-        self.head_dim = config.hidden_size // config.num_attention_heads
-        total_head_dim = (config.num_attention_heads + 2 * config.num_key_value_heads) * self.head_dim
-        # key, query, value projections for all heads, but in a batch
-        self.wqkv = FusedDense(config.hidden_size, total_head_dim, bias=config.attention_bias)
-        self.wo = FusedDense(config.hidden_size, config.hidden_size, bias=config.attention_bias)
-
-        self.num_heads = config.num_attention_heads
-        self.n_local_heads = config.num_key_value_heads
         self.hidden_size = config.hidden_size
+        self.head_dim = config.hidden_size // config.num_attention_heads
+        self.num_heads = config.num_attention_heads
+        self.num_key_value_heads = config.num_key_value_heads
+        self.num_key_value_groups = self.num_heads // self.num_key_value_heads
+
         if (self.head_dim * self.num_heads) != self.hidden_size:
             raise ValueError(
                 f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
                 f" and `num_heads`: {self.num_heads})."
             )
-        self._register_load_state_dict_pre_hook(self.load_hook)
-        cache_shape = (config.max_batch_size, self.n_local_heads, config.max_position_embeddings, self.head_dim)
+
+        self.q_proj = FusedDense(self.hidden_size, self.num_heads * self.head_dim, bias=False)
+        self.k_proj = FusedDense(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
+        self.v_proj = FusedDense(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
+        self.o_proj = FusedDense(self.num_heads * self.head_dim, self.hidden_size, bias=False)
+
+        cache_shape = (config.max_batch_size, config.max_position_embeddings, self.num_key_value_heads, self.head_dim)
         self.register_buffer('k_cache', torch.zeros(cache_shape, dtype=torch.bfloat16, device='cuda'), persistent=False)
         self.register_buffer('v_cache', torch.zeros(cache_shape, dtype=torch.bfloat16, device='cuda'), persistent=False)
 
-    def load_hook(self, state_dict, prefix, *args):
-        if prefix + "wq.weight" in state_dict:
-            wq = state_dict.pop(prefix + "wq.weight")
-            wk = state_dict.pop(prefix + "wk.weight")
-            wv = state_dict.pop(prefix + "wv.weight")
-            state_dict[prefix + "wqkv.weight"] = torch.cat([wq, wk, wv])
-
     @torch.no_grad()
-    def update_cache(self, input_pos, k_val, v_val):
-        # input_pos: [S], k_val: [B, H, S, D]
-        assert input_pos.shape[0] == k_val.shape[2], f"{input_pos.shape[0]} != {k_val.shape[2]}"
-
+    def update_cache(self, start_pos, k_val, v_val):
+        seqlen = k_val.size(1)
         k_out = self.k_cache
         v_out = self.v_cache
-        k_out[:, :, input_pos] = k_val
-        v_out[:, :, input_pos] = v_val
+        k_out[:, start_pos: start_pos + seqlen] = k_val
+        v_out[:, start_pos: start_pos + seqlen] = v_val
 
-        return k_out, v_out
+        return k_out[:, :seqlen], v_out[:, :seqlen]
 
-    def forward(self, x: Tensor, freqs_cis: Tensor, mask: Tensor, input_pos: Optional[Tensor] = None) -> Tensor:
-        bsz, seqlen, _ = x.shape
+    def forward(self, x: Tensor, mask: Tensor, freqs_cis: Tensor, start_pos: Optional[Tensor] = None, ) -> Tensor:
+        bsz, seqlen, _ = x.size()
 
-        kv_size = self.n_local_heads * self.head_dim
-        q, k, v = self.wqkv(x).split([self.hidden_size, kv_size, kv_size], dim=-1)
+        query_states = self.q_proj(x)
+        key_states = self.k_proj(x)
+        value_states = self.v_proj(x)
 
-        q = q.view(bsz, seqlen, self.num_heads, self.head_dim)
-        k = k.view(bsz, seqlen, self.n_local_heads, self.head_dim)
-        v = v.view(bsz, seqlen, self.n_local_heads, self.head_dim)
+        query_states = query_states.view(bsz, seqlen, self.num_heads, self.head_dim)
+        key_states = key_states.view(bsz, seqlen, self.num_key_value_heads, self.head_dim)
+        value_states = value_states.view(bsz, seqlen, self.num_key_value_heads, self.head_dim)
 
-        q = apply_rotary_emb(q, freqs_cis)
-        k = apply_rotary_emb(k, freqs_cis)
-
-        q, k, v = map(lambda x: x.transpose(1, 2), (q, k, v))
+        query_states, key_states = apply_rotary_emb(query_states, key_states, freqs_cis)
 
         if not self.training:
-            k, v = self.update_cache(input_pos, k, v)
+            key_states, value_states = self.update_cache(start_pos, key_states, value_states)
 
-        k = k.repeat_interleave(self.num_heads // self.n_local_heads, dim=1)
-        v = v.repeat_interleave(self.num_heads // self.n_local_heads, dim=1)
-        y = F.scaled_dot_product_attention(q, k, v, attn_mask=mask, dropout_p=0.0)
+        query_states, key_states, value_states = map(lambda x: x.transpose(1, 2),
+                                                     (query_states, key_states, value_states))
 
-        y = y.transpose(1, 2).contiguous().view(bsz, seqlen, self.hidden_size)
+        key_states = key_states.repeat_interleave(self.num_key_value_groups, dim=1)
+        value_states = value_states.repeat_interleave(self.num_key_value_groups, dim=1)
 
-        y = self.wo(y)
-        return y
+        attn_output = F.scaled_dot_product_attention(query_states, key_states, value_states,
+                                                     attn_mask=mask, dropout_p=0.0)
+
+        attn_output = attn_output.transpose(1, 2).contiguous().view(bsz, seqlen, self.hidden_size)
+
+        attn_output = self.o_proj(attn_output)
+        return attn_output

model/block.py
@@ -1,21 +1,32 @@
+from typing import Tuple
+
 import torch.nn as nn
 from flash_attn.ops.rms_norm import RMSNorm
 from torch import Tensor
 
 from model import ModelConfig
 from model.attention_layer import Attention
-from model.feed_forward import FeedForward
+from model.feed_forward import SparseMoEBlock
 
 
 class TransformerBlock(nn.Module):
     def __init__(self, config: ModelConfig) -> None:
         super().__init__()
         self.attention = Attention(config)
-        self.feed_forward = FeedForward(config)
-        self.ffn_norm = RMSNorm(config.hidden_size, config.rms_norm_eps)
-        self.attention_norm = RMSNorm(config.hidden_size, config.rms_norm_eps)
+        self.block_sparse_moe = SparseMoEBlock(config)
+        self.input_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps)
 
-    def forward(self, x: Tensor, input_pos: Tensor, freqs_cis: Tensor, mask: Tensor) -> Tensor:
-        h = x + self.attention(self.attention_norm(x), freqs_cis, mask, input_pos)
-        out = h + self.feed_forward(self.ffn_norm(h))
-        return out
+    def forward(self, x: Tensor, start_pos: Tensor, mask: Tensor, freqs_cis: Tensor) -> Tuple[Tensor, Tensor]:
+        # Self-attention
+        residual = x
+        x = self.input_layernorm(x)
+        x = residual + self.attention(x, mask, start_pos=start_pos, freqs_cis=freqs_cis)
+
+        # Block-sparse MoE
+        residual = x
+        x = self.post_attention_layernorm(x)
+        x, router_logits = self.block_sparse_moe(x)
+        x = residual + x
+
+        return x, router_logits

model/feed_forward.py
@@ -1,8 +1,8 @@
+import torch
 import torch.nn as nn
+from flash_attn.ops.fused_dense import FusedDense
 from torch import Tensor
 from torch.nn import functional as F
-
-from flash_attn.ops.fused_dense import FusedDense
 
 from model import ModelConfig
 
@@ -16,3 +16,49 @@ class FeedForward(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         return self.w2(F.silu(self.w1(x)) * self.w3(x))
+
+
+class SparseMoEBlock(nn.Module):
+    def __init__(self, config: ModelConfig) -> None:
+        super().__init__()
+        self.hidden_dim = config.hidden_size
+        self.ffn_dim = config.intermediate_size
+        self.num_experts = config.num_local_experts
+        self.top_k = config.num_experts_per_tok
+
+        self.gate = FusedDense(self.hidden_dim, self.num_experts, bias=False)
+
+        self.experts = nn.ModuleList([FeedForward(config) for _ in range(self.num_experts)])
+
+    def forward(self, x: Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        batch_size, sequence_length, hidden_dim = x.shape
+        x = x.reshape(-1, hidden_dim)
+        router_logits = self.gate(x)
+
+        routing_weights = F.softmax(router_logits, dim=-1, dtype=torch.float)
+        routing_weights, selected_experts = routing_weights.topk(self.top_k, dim=-1)
+        routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+
+        routing_weights = routing_weights.to(x.dtype)
+
+        final_hidden_states = torch.zeros((batch_size * sequence_length, hidden_dim), dtype=x.dtype, device=x.device)
+
+        expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+
+        for expert_idx in range(self.num_experts):
+            expert_layer = self.experts[expert_idx]
+            idx, top_x = torch.where(expert_mask[expert_idx])
+
+            if top_x.shape[0] == 0:
+                continue
+
+            top_x_list = top_x.tolist()
+            idx_list = idx.tolist()
+
+            current_state = x[None, top_x_list].reshape(-1, hidden_dim)
+            current_hidden_states = expert_layer(current_state) * routing_weights[top_x_list, idx_list, None]
+
+            final_hidden_states.index_add_(0, top_x, current_hidden_states.to(x.dtype))
+
+        final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
+        return final_hidden_states, router_logits

model/transformer.py
@@ -1,8 +1,9 @@
 import re
-from typing import Optional
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from flash_attn.ops.fused_dense import FusedDense
 from flash_attn.ops.rms_norm import RMSNorm
 from torch import Tensor
@@ -12,15 +13,35 @@ from model import ModelConfig, Tokenizer
 from model.block import TransformerBlock
 
 
-def precompute_freqs_cis(
-        seq_len: int, n_elem: int, base: float = 10000.0
-) -> Tensor:
-    freqs = 1.0 / (base ** (torch.arange(0, n_elem, 2)[: (n_elem // 2)].float() / n_elem))
-    t = torch.arange(seq_len, device=freqs.device)
-    freqs = torch.outer(t, freqs)
-    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
-    cache = torch.stack([freqs_cis.real, freqs_cis.imag], dim=-1)
-    return cache.to(dtype=torch.bfloat16)
+def load_balancing_loss_func(gate_logits: Tensor | Tuple, num_experts: int = None, top_k=2) -> float:
+    r"""
+    Computes auxiliary load balancing loss as in Switch Transformer - implemented in Pytorch.
+    """
+    if gate_logits is None or not isinstance(gate_logits, tuple):
+        return 0
+
+    if isinstance(gate_logits, tuple):
+        compute_device = gate_logits[0].device
+        concatenated_gate_logits = torch.cat([layer_gate.to(compute_device) for layer_gate in gate_logits], dim=0)
+
+    routing_weights = torch.nn.functional.softmax(concatenated_gate_logits, dim=-1)
+
+    _, selected_experts = torch.topk(routing_weights, top_k, dim=-1)
+
+    # treat `top_k` as tokens (shape is `top_k X [batch_size X sequence_length]`)
+    selected_experts = selected_experts.reshape(-1)
+
+    expert_mask = torch.nn.functional.one_hot(selected_experts, num_experts)
+    expert_mask = torch.max(expert_mask, dim=-2).values
+
+    # Compute the percentage of tokens routed to each experts
+    tokens_per_expert = torch.mean(expert_mask.float(), dim=0)
+
+    # Compute the average probability of routing to these experts
+    router_prob_per_expert = torch.mean(routing_weights, dim=0)
+
+    overall_loss = torch.sum(tokens_per_expert * router_prob_per_expert.unsqueeze(-1))
+    return overall_loss * num_experts
 
 
 def multinomial_sample_one_no_sync(probs_sort):  # Does multinomial sampling without a cuda synchronization
@@ -45,6 +66,14 @@ def sample(logits, temperature: float = 1.0, top_k: Optional[int] = None):
     return idx_next, probs
 
 
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0) -> Tensor:
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
+    return freqs_cis
+
+
 class Transformer(nn.Module):
     def __init__(self, config: ModelConfig) -> None:
         super().__init__()
@@ -56,16 +85,16 @@ class Transformer(nn.Module):
         self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.output = FusedDense(config.hidden_size, config.vocab_size, bias=False)
 
-        self.freqs_cis: Optional[Tensor] = None
-        self.mask_cache: Optional[Tensor] = None
         self.max_batch_size = config.max_batch_size
         self.max_seq_length = config.max_position_embeddings
         self.gradient_checkpointing = config.gradient_checkpointing
+        self.router_aux_loss_coef = config.router_aux_loss_coef
+        self.num_experts = config.num_local_experts
 
-        self.freqs_cis = precompute_freqs_cis(self.config.max_position_embeddings,
-                                              self.config.hidden_size // self.config.num_attention_heads,
-                                              self.config.rope_theta).cuda()
         self.causal_mask = torch.tril(torch.ones(self.max_seq_length, self.max_seq_length, dtype=torch.bool)).cuda()
+        self.freqs_cis = precompute_freqs_cis(self.config.hidden_size // self.config.num_attention_heads,
+                                              self.config.max_position_embeddings * 2,
+                                              self.config.rope_theta).cuda()
 
     def _init_weights(self, module):
         std = self.config.initializer_range
@@ -78,32 +107,42 @@ class Transformer(nn.Module):
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
 
-    def forward(self, idx: Tensor, input_pos: Optional[Tensor] = None) -> Tensor:
-        assert self.freqs_cis is not None, "Caches must be initialized first"
-        if input_pos is None:
-            device = idx.device
-            input_pos = torch.arange(0, idx.shape[-1], dtype=torch.long, device=device)
-        mask = self.causal_mask[None, None, input_pos]
-        freqs_cis = self.freqs_cis[input_pos]
-        x = self.tok_embeddings(idx)
+    def forward(self, x: Tensor, y: Optional[Tensor] = None, start_pos: int = 0) -> tuple[Tensor, Optional[Tensor]]:
+        seq_length = x.shape[1]
+        mask = self.causal_mask[:seq_length, :seq_length]
+        x = self.tok_embeddings(x)
+        freqs_cis = self.freqs_cis[start_pos: start_pos + seq_length]
 
+        all_router_logits = ()
         for i, layer in enumerate(self.layers):
             if self.gradient_checkpointing and self.training:
-                x = checkpoint(layer.__call__, x, input_pos, freqs_cis, mask, use_reentrant=True)
+                x, router_logits = checkpoint(layer.__call__, x, start_pos, mask, freqs_cis, use_reentrant=False)
             else:
-                x = layer(x, input_pos, freqs_cis, mask)
+                x, router_logits = layer(x, start_pos, mask, freqs_cis=freqs_cis)
+            all_router_logits += (router_logits,)
         x = self.norm(x)
         logits = self.output(x)
-        return logits
+        loss = None
+        if y is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1), ignore_index=-1)
+            aux_loss = load_balancing_loss_func(all_router_logits, self.num_experts, top_k=2)
+            loss += self.router_aux_loss_coef * aux_loss
+
+        return logits, loss
 
     @torch.no_grad()
-    def generate(self, prompt: torch.Tensor, tokenizer: Tokenizer, max_tokens: int, stream: bool = True,
+    def generate(self, prompt: Tensor, tokenizer: Tokenizer, max_tokens: int, stream: bool = True,
                  **sampling_kwargs) -> str:
         return_output = ''
-        while len(prompt) < max_tokens:
-            logits = self(prompt.view(1, -1))
+        prev_pos = 0
+        tokens = torch.full((1, max_tokens), 0, dtype=torch.long, device="cuda")
+        tokens[:, :prompt.shape[-1]] = prompt
+        for cur_pos in range(prompt.shape[-1], max_tokens):
+            logits, _ = self(tokens[:, prev_pos: cur_pos], start_pos=prev_pos)
             idx_next, _ = sample(logits, **sampling_kwargs)
             idx = idx_next.tolist()[0]
+            tokens[:, cur_pos] = idx
+            prev_pos = cur_pos
             if idx == tokenizer.eos_id or idx == tokenizer.pad_id:
                 break
             out = tokenizer.decode_piece(idx)

prompt.py
@@ -11,15 +11,15 @@ def get_prompt(conv: list[dict[str, str]]) -> list[int]:
     for line in conv:
         for k, v in line.items():
             if k == 'SYSTEM':
-                tokens += tokenizer.encode(f"###{k}: {v}\n", bos=True, eos=True)
+                tokens += tokenizer.encode(f" ### System: {v} ", bos=True, eos=True)
             elif k == 'USER':
-                tokens += tokenizer.encode(f"\n###{k}: {v}\n", eos=True, bos=True if not tokens else False)
+                tokens += tokenizer.encode(f" ### Instruction: {v} ", eos=True, bos=True if not tokens else False)
             elif k == 'ASSISTANT':
-                tokens += tokenizer.encode(f"\n###{k}: {v}\n", eos=True, bos=False)
+                tokens += tokenizer.encode(f" ### Response: {v} ", eos=True, bos=False)
             else:
                 raise ValueError(f"Unknown key {k}")
     if conv[-1].keys() == {'USER'}:
-        tokens += tokenizer.encode('\n###ASSISTANT:', eos=False, bos=False)
+        tokens += tokenizer.encode(' ### Response: ', eos=False, bos=False)
     return tokens
 
 
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     model = model.eval()
 
     CONVERSATION: list[dict[str, str]] = [
-        {"SYSTEM": "You are an AI Assistant. Respond to USER prompts."},
+        # {"SYSTEM": "You are an AI Assistant. Write response to the given instructions. Follow the instructions carefully."},
     ]
 
 
@@ -57,6 +57,6 @@ if __name__ == '__main__':
             break
         CONVERSATION.append({"USER": inp})
         prompt = torch.tensor(get_prompt(CONVERSATION), dtype=torch.int, device=device)
-        out = model.generate(prompt, tokenizer=tokenizer, max_tokens=128, stream=False, temperature=1.0, top_k=20)
+        out = model.generate(prompt, tokenizer=tokenizer, max_tokens=1024, stream=False, temperature=1.0, top_k=16)
         CONVERSATION.append({"ASSISTANT": out.strip()})
         print(f"ASSISTANT: {out.strip()}")