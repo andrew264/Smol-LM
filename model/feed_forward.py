fc5664ca2bf1482c37d5a26cef0f9ecb2190c4a9
Andrew Santhosh C <andrewsanthosh264@gmail.com>
2023-12-24 17:13:56 +0530

    update FFN

--- model/feed_forward.py
+++ model/feed_forward.py
@@ -11,11 +11,10 @@ class FeedForward(nn.Module):
     def __init__(self, config: ModelConfig) -> None:
         super().__init__()
         self.w1 = FusedDense(config.hidden_size, config.intermediate_size, bias=False)
-        self.w3 = FusedDense(config.hidden_size, config.intermediate_size, bias=False)
         self.w2 = FusedDense(config.intermediate_size, config.hidden_size, bias=False)
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.w2(F.silu(self.w1(x)) * self.w3(x))
+        return self.w2(F.silu(self.w1(x)))
 
 
 class SparseMoEBlock(nn.Module):
