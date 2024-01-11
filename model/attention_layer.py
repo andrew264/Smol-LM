flash attention 2 fix attention mask [i was living with a lie]

model/attention_layer.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
+from flash_attn import flash_attn_func, flash_attn_varlen_func
+from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input
 from flash_attn.ops.fused_dense import FusedDense
 from torch import Tensor
 from torch.nn import functional as F
@@ -27,6 +29,14 @@ def apply_rotary_emb(xq: Tensor, xk: Tensor, freqs_cis: Tensor) -> tuple[Tensor,
     xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
     xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
     return xq_out.type_as(xq), xk_out.type_as(xk)
+
+
+def _get_unpad_data(attention_mask):
+    seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
+    indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
+    max_seqlen_in_batch = seqlens_in_batch.max().item()
+    cu_seqlens = F.pad(torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.torch.int32), (1, 0))
+    return indices, cu_seqlens, max_seqlen_in_batch,
 
 
 class Attention(nn.Module):
@@ -69,7 +79,8 @@ class Attention(nn.Module):
 
         return keys, values
 
-    def forward(self, x: Tensor, mask: Tensor, freqs_cis: Tensor, start_pos: Optional[Tensor] = None, ) -> Tensor:
+    def forward(self, x: Tensor, mask: Optional[Tensor],
+                freqs_cis: Tensor, start_pos: Optional[Tensor] = None, ) -> Tensor:
         bsz, seqlen, _ = x.size()
 
         query_states: Tensor = self.q_proj(x)
@@ -85,21 +96,124 @@ class Attention(nn.Module):
         if not self.training:
             key_states, value_states = self.update_cache(start_pos, key_states, value_states)
 
-        query_states, key_states, value_states = map(lambda x: x.transpose(1, 2),
-                                                     (query_states, key_states, value_states))
-
-        key_states = key_states.repeat_interleave(self.num_key_value_groups, dim=1)
-        value_states = value_states.repeat_interleave(self.num_key_value_groups, dim=1)
-
         if query_states.device.type == "cuda" and mask is not None:
             query_states = query_states.contiguous()
             key_states = key_states.contiguous()
             value_states = value_states.contiguous()
 
-        attn_output = F.scaled_dot_product_attention(query_states, key_states, value_states,
-                                                     attn_mask=mask, dropout_p=0.0)
+        kv_seq_len = key_states.shape[1]
+        use_sliding_windows = 0 < self.config.sliding_window < kv_seq_len
+        attn_output = self._flash_attn_forward(query_states, key_states, value_states, mask, seqlen, dropout=0.0,
+                                               softmax_scale=None, use_sliding_windows=use_sliding_windows, )
 
-        attn_output = attn_output.transpose(1, 2).contiguous().view(bsz, seqlen, self.hidden_size)
+        attn_output = attn_output.contiguous().view(bsz, seqlen, self.hidden_size)
 
         attn_output = self.o_proj(attn_output)
         return attn_output
+
+    def _flash_attn_forward(self, query_states, key_states, value_states, attention_mask, query_length, dropout=0.0,
+                            softmax_scale=None, use_sliding_windows=False, ) -> Tensor:
+        causal = True
+        if attention_mask is not None:
+            batch_size = query_states.shape[0]
+            query_states, key_states, value_states, indices_q, cu_seq_lens, max_seq_lens = self._upad_input(
+                query_states, key_states, value_states, attention_mask, query_length
+            )
+
+            cu_seqlens_q, cu_seqlens_k = cu_seq_lens
+            max_seqlen_in_batch_q, max_seqlen_in_batch_k = max_seq_lens
+
+            if not use_sliding_windows:
+                attn_output_unpad = flash_attn_varlen_func(
+                    query_states,
+                    key_states,
+                    value_states,
+                    cu_seqlens_q=cu_seqlens_q,
+                    cu_seqlens_k=cu_seqlens_k,
+                    max_seqlen_q=max_seqlen_in_batch_q,
+                    max_seqlen_k=max_seqlen_in_batch_k,
+                    dropout_p=dropout,
+                    softmax_scale=softmax_scale,
+                    causal=causal,
+                )
+            else:
+                attn_output_unpad = flash_attn_varlen_func(
+                    query_states,
+                    key_states,
+                    value_states,
+                    cu_seqlens_q=cu_seqlens_q,
+                    cu_seqlens_k=cu_seqlens_k,
+                    max_seqlen_q=max_seqlen_in_batch_q,
+                    max_seqlen_k=max_seqlen_in_batch_k,
+                    dropout_p=dropout,
+                    softmax_scale=softmax_scale,
+                    causal=causal,
+                    window_size=(self.config.sliding_window, self.config.sliding_window),
+                )
+
+            attn_output = pad_input(attn_output_unpad, indices_q, batch_size, query_length)
+        else:
+            if not use_sliding_windows:
+                attn_output = flash_attn_func(
+                    query_states,
+                    key_states,
+                    value_states,
+                    dropout,
+                    softmax_scale=softmax_scale,
+                    causal=causal,
+                )
+            else:
+                attn_output = flash_attn_func(
+                    query_states,
+                    key_states,
+                    value_states,
+                    dropout,
+                    softmax_scale=softmax_scale,
+                    causal=causal,
+                    window_size=(self.config.sliding_window, self.config.sliding_window),
+                )
+
+        return attn_output
+
+    @staticmethod
+    def _upad_input(query_layer, key_layer, value_layer, attention_mask, query_length):
+        batch_size, kv_seq_len, num_heads, head_dim = key_layer.shape
+
+        # On the first iteration we need to properly re-create the padding mask
+        # by slicing it on the proper place
+        if kv_seq_len != attention_mask.shape[-1]:
+            attention_mask_num_tokens = attention_mask.shape[-1]
+            attention_mask = attention_mask[:, attention_mask_num_tokens - kv_seq_len:]
+
+        indices_k, cu_seqlens_k, max_seqlen_in_batch_k = _get_unpad_data(attention_mask)
+
+        key_layer = index_first_axis(key_layer.reshape(batch_size * kv_seq_len, num_heads, head_dim), indices_k)
+        value_layer = index_first_axis(value_layer.reshape(batch_size * kv_seq_len, num_heads, head_dim), indices_k)
+
+        if query_length == kv_seq_len:
+            query_layer = index_first_axis(
+                query_layer.reshape(batch_size * kv_seq_len, num_heads, head_dim), indices_k
+            )
+            cu_seqlens_q = cu_seqlens_k
+            max_seqlen_in_batch_q = max_seqlen_in_batch_k
+            indices_q = indices_k
+        elif query_length == 1:
+            max_seqlen_in_batch_q = 1
+            cu_seqlens_q = torch.arange(
+                batch_size + 1, dtype=torch.int32, device=query_layer.device
+            )  # There is a memcpy here, that is very bad.
+            indices_q = cu_seqlens_q[:-1]
+            query_layer = query_layer.squeeze(1)
+        else:
+            # The -q_len: slice assumes left padding.
+            attention_mask = attention_mask[:, -query_length:]
+            query_layer, indices_q, cu_seqlens_q, max_seqlen_in_batch_q = unpad_input(query_layer, attention_mask)
+
+        return (
+            query_layer,
+            key_layer,
+            value_layer,
+            indices_q,
+            (cu_seqlens_q, cu_seqlens_k),
+            (max_seqlen_in_batch_q, max_seqlen_in_batch_k),
+        )

model/block.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import torch.nn as nn
 from flash_attn.ops.rms_norm import RMSNorm
@@ -21,7 +21,7 @@ class TransformerBlock(nn.Module):
         self.input_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps)
         self.post_attention_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps)
 
-    def forward(self, x: Tensor, start_pos: Tensor, mask: Tensor, freqs_cis: Tensor) -> Tuple[Tensor, Tensor]:
+    def forward(self, x: Tensor, start_pos: Tensor, mask: Optional[Tensor], freqs_cis: Tensor) -> Tuple[Tensor, Tensor]:
         # Self-attention
         residual = x
         x = self.input_layernorm(x)

model/transformer.py
@@ -90,12 +90,10 @@ class Transformer(nn.Module):
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
 
-    def forward(self, x: Tensor, y: Optional[Tensor] = None, start_pos: int = 0) -> tuple[Tensor, Optional[Tensor]]:
+    def forward(self, x: Tensor, y: Optional[Tensor] = None, start_pos: int = 0,
+                mask: Optional[Tensor] = None) -> tuple[Tensor, Optional[Tensor]]:
         seq_length = x.shape[1]
-        if seq_length > 1:
-            mask = self.causal_mask[:seq_length, :seq_length]
-        else:
-            mask = None
+
         x = self.tok_embeddings(x)
         x = self.embedding_norm(x)
         freqs_cis = self.freqs_cis[start_pos: start_pos + seq_length]
@@ -129,20 +127,17 @@ class Transformer(nn.Module):
         for cur_pos in range(prompt.shape[-1], max_tokens):
             logits, _ = self(tokens[:, prev_pos: cur_pos], start_pos=prev_pos)
             logits = logits[:, -1]
+            logits = F.log_softmax(logits, dim=-1)
 
             if logits_processors is not None:
-                for processor in logits_processors:
-                    logits = processor(input_ids=tokens[:, ], scores=logits)
+                logits = logits_processors(input_ids=tokens[:, ], scores=logits)
 
                 probs = F.softmax(logits, dim=-1)
                 next_token = torch.multinomial(probs, num_samples=1)
             else:
                 next_token = torch.argmax(logits, dim=-1, keepdim=True)
 
-            next_token = next_token.reshape(-1)
-            idx_next = next_token[-1].unsqueeze(0)
-
-            idx = idx_next.tolist()[0]
+            idx = next_token.item()
             tokens[:, cur_pos] = idx
             prev_pos = cur_pos
             if idx in [pad_id, eos_id]:
@@ -152,7 +147,7 @@ class Transformer(nn.Module):
                 if tokenizer is None:
                     raise ValueError("Tokenizer must be provided if stream is True.")
                 print(tokenizer.decode([idx]), end='', flush=True)
-            prompt = torch.cat([prompt, idx_next], dim=-1)
+            prompt = torch.cat([prompt, next_token.squeeze(1)], dim=-1)
         if stream:
             print()
         return return_output