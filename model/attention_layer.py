f83a8d5408aa0baeca9bcb9e242d3feebc70e2e9
Andrew Santhosh C <andrewsanthosh264@gmail.com>
2024-01-14 14:57:21 +0530

    FA-2 attention mask changes

--- model/attention_layer.py
+++ model/attention_layer.py
@@ -2,8 +2,8 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
-from flash_attn import flash_attn_func, flash_attn_varlen_func
-from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input
+from flash_attn import flash_attn_func, flash_attn_varlen_qkvpacked_func
+from flash_attn.bert_padding import pad_input, unpad_input
 from flash_attn.ops.fused_dense import FusedDense
 from torch import Tensor
 from torch.nn import functional as F
@@ -29,14 +29,6 @@ def apply_rotary_emb(xq: Tensor, xk: Tensor, freqs_cis: Tensor) -> tuple[Tensor,
     xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
     xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
     return xq_out.type_as(xq), xk_out.type_as(xk)
-
-
-def _get_unpad_data(attention_mask):
-    seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
-    indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
-    max_seqlen_in_batch = seqlens_in_batch.max().item()
-    cu_seqlens = F.pad(torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.torch.int32), (1, 0))
-    return indices, cu_seqlens, max_seqlen_in_batch,
 
 
 class Attention(nn.Module):
@@ -82,7 +74,7 @@ class Attention(nn.Module):
 
         if self.training:
             use_sliding_windows = 0 < self.config.sliding_window < kv_seq_len
-            attn_output = self._flash_attn_forward(query_states, key_states, value_states, mask, seqlen,
+            attn_output = self._flash_attn_forward(query_states, key_states, value_states, mask,
                                                    use_sliding_windows=use_sliding_windows, )
         else:
             # key value caching
@@ -96,7 +88,7 @@ class Attention(nn.Module):
             self.v_cache = torch.cat([self.v_cache, value_states], dim=1)
             use_sliding_windows = 0 < self.config.sliding_window < self.k_cache.shape[1]
 
-            attn_output = self._flash_attn_forward(query_states, self.k_cache, self.v_cache, mask, seqlen,
+            attn_output = self._flash_attn_forward(query_states, self.k_cache, self.v_cache, mask,
                                                    use_sliding_windows=use_sliding_windows, )
 
         attn_output = attn_output.contiguous().view(bsz, seqlen, self.hidden_size)
@@ -104,86 +96,34 @@ class Attention(nn.Module):
         attn_output = self.o_proj(attn_output)
         return attn_output
 
-    def _flash_attn_forward(self, query_states, key_states, value_states, attention_mask, query_length, dropout=0.0,
-                            softmax_scale=None, use_sliding_windows=False, ) -> Tensor:
+    def _flash_attn_forward(self, query_states, key_states, value_states, attention_mask, dropout=0.0,
+                            use_sliding_windows=False, ) -> Tensor:
         causal = True
         sliding_window = (self.config.sliding_window, self.config.sliding_window) if use_sliding_windows else (-1, -1)
         if attention_mask is not None:
-            batch_size = query_states.shape[0]
-            query_states, key_states, value_states, indices_q, cu_seq_lens, max_seq_lens = self._upad_input(
-                query_states, key_states, value_states, attention_mask, query_length
-            )
-
-            cu_seqlens_q, cu_seqlens_k = cu_seq_lens
-            max_seqlen_in_batch_q, max_seqlen_in_batch_k = max_seq_lens
-
-            attn_output_unpad = flash_attn_varlen_func(
-                query_states,
-                key_states,
-                value_states,
-                cu_seqlens_q=cu_seqlens_q,
-                cu_seqlens_k=cu_seqlens_k,
-                max_seqlen_q=max_seqlen_in_batch_q,
-                max_seqlen_k=max_seqlen_in_batch_k,
+            bsz, q_len, _, _ = query_states.size()
+            qkv = torch.stack([query_states, key_states, value_states], dim=2)
+            qkv = qkv.reshape(bsz, q_len, -1)
+            qkv, indices, cu_q_lens, max_s = unpad_input(qkv, attention_mask)
+            qkv = qkv.view(-1, 3, self.num_heads, self.head_dim)
+            attn_output_unpad = flash_attn_varlen_qkvpacked_func(
+                qkv,
+                cu_q_lens,
+                max_s,
                 dropout_p=dropout,
-                softmax_scale=softmax_scale,
-                causal=causal,
+                causal=True,
                 window_size=sliding_window
             )
-
-            attn_output = pad_input(attn_output_unpad, indices_q, batch_size, query_length)
+            attn_output_unpad = attn_output_unpad.reshape(-1, self.num_heads * self.head_dim)
+            attn_output = pad_input(attn_output_unpad, indices, bsz, q_len)
         else:
             attn_output = flash_attn_func(
                 query_states,
                 key_states,
                 value_states,
                 dropout,
-                softmax_scale=softmax_scale,
                 causal=causal,
                 window_size=sliding_window
             )
 
         return attn_output
-
-    @staticmethod
-    def _upad_input(query_layer, key_layer, value_layer, attention_mask, query_length):
-        batch_size, kv_seq_len, num_heads, head_dim = key_layer.shape
-
-        # On the first iteration we need to properly re-create the padding mask
-        # by slicing it on the proper place
-        if kv_seq_len != attention_mask.shape[-1]:
-            attention_mask_num_tokens = attention_mask.shape[-1]
-            attention_mask = attention_mask[:, attention_mask_num_tokens - kv_seq_len:]
-
-        indices_k, cu_seqlens_k, max_seqlen_in_batch_k = _get_unpad_data(attention_mask)
-
-        key_layer = index_first_axis(key_layer.reshape(batch_size * kv_seq_len, num_heads, head_dim), indices_k)
-        value_layer = index_first_axis(value_layer.reshape(batch_size * kv_seq_len, num_heads, head_dim), indices_k)
-
-        if query_length == kv_seq_len:
-            query_layer = index_first_axis(
-                query_layer.reshape(batch_size * kv_seq_len, num_heads, head_dim), indices_k
-            )
-            cu_seqlens_q = cu_seqlens_k
-            max_seqlen_in_batch_q = max_seqlen_in_batch_k
-            indices_q = indices_k
-        elif query_length == 1:
-            max_seqlen_in_batch_q = 1
-            cu_seqlens_q = torch.arange(
-                batch_size + 1, dtype=torch.int32, device=query_layer.device
-            )  # There is a memcpy here, that is very bad.
-            indices_q = cu_seqlens_q[:-1]
-            query_layer = query_layer.squeeze(1)
-        else:
-            # The -q_len: slice assumes left padding.
-            attention_mask = attention_mask[:, -query_length:]
-            query_layer, indices_q, cu_seqlens_q, max_seqlen_in_batch_q = unpad_input(query_layer, attention_mask)
-
-        return (
-            query_layer,
-            key_layer,
-            value_layer,
-            indices_q,
-            (cu_seqlens_q, cu_seqlens_k),
-            (max_seqlen_in_batch_q, max_seqlen_in_batch_k),
-        )
